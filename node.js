3711a4407ad8fbc11fd5138bd32c1dc2dd988060
luaroxy <luannamsiqueira@gmail.com>
2022-10-15 14:59:34 -0600

    Created Node and Tree class

---
+++ node.js
@@ -0,0 +1,9 @@
+export default class Node
+{
+  constructor(data)
+  {
+    this.data = data;
+    this.leftChild = null;
+    this.rightChild = null;
+  }
+} 
---
+++ tree.js
@@ -0,0 +1,11 @@
+export default class Tree
+{
+  constructor(arr)
+  {
+    this.root = this.buildTree();
+  }
+
+  buildTree(){
+    
+  }
+} 
